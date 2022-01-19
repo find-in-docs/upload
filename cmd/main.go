565fe5d9dfce70fb808c9c36d56ac2c994abeca6
Samir Gadkari <samir.gadkari@gmail.com>
2022-01-19 13:33:53 -0800

    Write int-to-word and word-to-int mappings to files

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"path/filepath"
 
 	"github.com/samirgadkari/search/pkg/config"
 	"github.com/samirgadkari/search/pkg/transform"
@@ -27,5 +26,5 @@ func main() {
 	fmt.Println(stopWords)
 
 	transform.WordsToInts(stopWords, cfg.DataFile,
-		filepath.Join(cfg.OutputDir, cfg.WordIntsFile))
+		cfg.OutputDir, cfg.WordIntsFile)
 }
